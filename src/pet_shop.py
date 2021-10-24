cdccfa339ed402c17281485ea014885d0a96305e
Louis Fletcher <Louisco1999@gmail.com>
2021-10-25 00:48:30 +0100

    altered some hw answers

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -35,14 +35,11 @@ def find_pet_by_name(pet_shop, pet_name):
 
 def remove_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop['pets']:
-        if pet_name == pet['name']:
+        if pet['name'] == pet_name:
             pet_shop['pets'].remove(pet)
 
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
-
-def get_stock_count(pet_shop):
-    return len(pet_shop["pets"])
 
 def get_customer_cash(customer):
     return customer["cash"]
